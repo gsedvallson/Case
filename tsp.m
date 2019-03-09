60e1c4924a7bd66023fda887fd97b914cfd62e7f
Gustav Sedvallson <gusse382@student.liu.se>
2019-03-09 15:38:09 +0100

    snääälla funka

--- tsp.m
+++ tsp.m
@@ -9,7 +9,7 @@ Y_cordStop=0;
 TourCost = zeros(length(stop_comb),1);
 
 A = ones(131071,1);
-%lÃ¤gg in som fÃ¶rsta kolumn i stop_comb!!!
+%lägg in som första kolumn i stop_comb!!! 
 
 
 for h = 1:length(stop_comb)
@@ -34,91 +34,72 @@ for h = 1:length(stop_comb)
         if(route(3,v1)==0)
             cords(:,v)=[];
             v = v - 1;
-            
-            if (v1 == length(route))
-                break;
-            end
-            v = v + 1;
-            v1 = v1 + 1;
-            
         end
         
-        counter=0;
-        d = 0;
-        iMin=0;
-        jMin =0;
-        steps = length(cords);
-        number_of_moves = 50*steps;
-        
-        
-        while (counter<number_of_moves)
-            dMin=10000000;
-            for k=1:(steps-2)
-                for l=(k+3):(steps)
-                    d = evalMove(k,l,cords);
-                    if(d<dMin)
-                        iMin = k;
-                        jMin = l;
-                        dMin = d;
-                    end
+        if (v == length(route))
+            break;
+        end
+        v = v + 1;
+        v1 = v1 + 1;
+    end
+    
+    
+    counter=0;
+    d = 0;
+    iMin=0;
+    jMin =0;
+    steps = length(cords);
+    number_of_moves = 50*steps;
+    
+    
+    while (counter<number_of_moves)
+        dMin=10000000;
+        for k=1:(steps-2)
+            for l=(k+3):(steps)
+                d = evalMove(k,l,cords);
+                if(d<dMin)
+                    iMin = k;
+                    jMin = l;
+                    dMin = d;
                 end
             end
-            cords = makeMove(iMin,jMin,cords);
-            counter = counter+1;
-            if dMin >= 0
-                break;
-            end
-            
+        end
+        cords = makeMove(iMin,jMin,cords);
+        counter = counter+1;
+        if dMin >= 0
+            break;
         end
         
-        x2 = cords(1,:);
-        y2 = cords(2,:);
-        
-        %     figure(1)
-        %     plot([x2 x2(1)],[y2 y2(1)],'k+:')
-        %
-        %     figure(2)
-        %     plot([x x(1)],[y y(1)],'k+:')
-        
-        D = zeros(length(x2),1);
-        if(size(cords,2) > 1)
-            for i=1:length(x2)-1
-                D(i) = sqrt(sum((x2(i) - x2(i+1)) .^ 2 + (y2(i) - y2(i+1)) .^ 2));
-            end
-            D(end) = sqrt(sum((x2(end) - x2(1)) .^ 2 + (y2(end) - y2(1)) .^ 2));
+    end
+    
+    x2 = cords(1,:);
+    y2 = cords(2,:);
+    
+    %     figure(1)
+    %     plot([x2 x2(1)],[y2 y2(1)],'k+:')
+    %
+    %     figure(2)
+    %     plot([x x(1)],[y y(1)],'k+:')
+    
+    D = zeros(length(x2),1);
+    if(size(cords,2) > 1)
+        for i=1:length(x2)-1
+            D(i) = sqrt(sum((x2(i) - x2(i+1)) .^ 2 + (y2(i) - y2(i+1)) .^ 2));
         end
-        
+        D(end) = sqrt(sum((x2(end) - x2(1)) .^ 2 + (y2(end) - y2(1)) .^ 2));
+    end
+    
+%         
 %         if (h == 10)
 %             disp("route: " + route);
 %             disp("cords " + cords);
 %             disp("D: " + D);
 %             disp("distD: " + sum(D));
 %         end
-        
-        TourCost(h) = sum(D);
-        % StartCost = sum(D);
-        % TourCost = sum(D2);
-        
-    end
+    
+    TourCost(h) = sum(D);
+    % StartCost = sum(D);
+    % TourCost = sum(D2);
+    
 end
 toc
-%% Här lägger vi in nya stopp!
-clear;
-tic;
-d=(dec2bin(1:262143));
-stop_ny = zeros(262143,18);
-
-for i=1:length(d)
-    for j=1:18
-        stop_ny(i,j) = str2num(d(i,j));
-    end
-end
-
-
-toc;
-
-%%
-clear;
-
-
-
